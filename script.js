cd90e2c21b1c22e2f0e454cce8dba7acd87ce880
AnnaKompan <annakompanch@gmail.com>
2024-11-01 21:37:40 +0800

    added map
    dataset from https://ourworldindata.org/grapher/refugee-population-by-country-or-territory-of-origin

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 const width = 800;
 const height = 500;
-
-// Define color scale
 const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
 
-// Create SVG container
 const svg = d3
   .select("#map")
   .append("svg")
   .attr("width", width)
   .attr("height", height);
 
-// Tooltip div
 const tooltip = d3
   .select("body")
   .append("div")
@@ -24,7 +20,6 @@ const tooltip = d3
   .style("pointer-events", "none")
   .style("opacity", 0);
 
-// Define map projection
 const projection = d3
   .geoMercator()
   .scale(130)
@@ -32,47 +27,77 @@ const projection = d3
 
 const path = d3.geoPath().projection(projection);
 
-// Load and process data
-Promise.all([
-  d3.json("https://unpkg.com/world-atlas@2/countries-50m.json"), // World map data
-  d3.csv("data.csv"), // Asylum application data
-]).then(([worldData, asylumData]) => {
-  // Convert TopoJSON to GeoJSON
-  const countries = topojson.feature(
-    worldData,
-    worldData.objects.countries
-  ).features;
+let asylumData;
+let countries;
+let selectedYear = "2014";
 
-  // Prepare data for tooltips
+const filterDataByYear = (year) => {
   const asylumApplications = {};
   asylumData.forEach((d) => {
-    const country = d["Country of origin"];
-    const asylumCountry = d["Country of asylum"];
-    const applied = +d.applied;
-    asylumApplications[country] = { asylumCountry, applied };
+    if (d.Year === year) {
+      const country = d["Country of origin"];
+      const refugees = +d["Refugees under UNHCR's mandate"];
+      asylumApplications[country] = { refugees };
+    }
   });
+  return asylumApplications;
+};
 
-  // Draw the map
+const yearDropdown = d3.select("#year").append("select");
+yearDropdown.on("change", function () {
+  selectedYear = this.value;
+  updateMap();
+});
+
+Promise.all([
+  d3.json("https://unpkg.com/world-atlas@2/countries-50m.json"),
+  d3.csv("refugees.csv"),
+]).then(([worldData, csvData]) => {
+  asylumData = csvData;
+  countries = topojson.feature(worldData, worldData.objects.countries).features;
+
+  const years = Array.from(new Set(asylumData.map((d) => d.Year)));
+  yearDropdown
+    .selectAll("option")
+    .data(years)
+    .enter()
+    .append("option")
+    .attr("value", (d) => d)
+    .text((d) => d);
+
+  updateMap();
+});
+
+function updateMap() {
+  const asylumApplications = filterDataByYear(selectedYear);
+  const unknownRefugeeCountries = []; // array to store countries with unknown data
+
   svg
     .selectAll("path")
     .data(countries)
-    .enter()
-    .append("path")
+    .join("path")
     .attr("d", path)
-    .attr("fill", (d) => colorScale(Math.random() * 10))
+    .attr("fill", (d) => {
+      const countryName = d.properties.name;
+      const asylumData = asylumApplications[countryName];
+
+      // Check if refugee data is unknown and add to the list
+      if (!asylumData) {
+        unknownRefugeeCountries.push(countryName);
+      }
+
+      return asylumData ? colorScale(asylumData.refugees) : "#ccc";
+    })
     .attr("stroke", "#999")
     .attr("stroke-width", 0.5)
     .on("mouseover", function (event, d) {
       const countryName = d.properties.name;
       const asylumData = asylumApplications[countryName];
-      const asylumCountry = asylumData ? asylumData.asylumCountry : "Unknown";
-      const applied = asylumData ? asylumData.applied : "Unknown";
+      const refugees = asylumData ? asylumData.refugees : "Unknown";
 
       tooltip.transition().duration(200).style("opacity", 1);
       tooltip
-        .html(
-          `<strong>${countryName}</strong><br>Country of Asylum: ${asylumCountry}<br>Applications: ${applied}`
-        )
+        .html(`<strong>${countryName}</strong><br>Refugees: ${refugees}`)
         .style("left", event.pageX + 10 + "px")
         .style("top", event.pageY - 20 + "px");
     })
@@ -84,4 +109,7 @@ Promise.all([
     .on("mouseout", function () {
       tooltip.transition().duration(200).style("opacity", 0);
     });
-});
+
+  // Log the countries with unknown refugee data
+  console.log("Countries with unknown refugee data:", unknownRefugeeCountries);
+}
